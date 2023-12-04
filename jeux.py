14d67a60f4275c290a34182628823261c555d086
thomasGarau <thomas.garau8@gmail.com>
2023-12-04 14:42:02 +0100

    fin implémentation minMax et alphaBeta

--- jeux.py
+++ jeux.py
@@ -3,7 +3,7 @@ import json
 class KalahGame:
     def __init__(self):
         # Initialize the game board
-        self.board = [0] + [3] * 6 + [0] + [3] * 6
+        self.board = [0] + [8] * 6 + [0] + [8] * 6
         self.level = None
         #0 sud 1 nord
         self.player_side = None
@@ -13,19 +13,19 @@ class KalahGame:
        
     def display_board(self):
         #clear the user terminal 
-        os.system('cls')
+        #os.system('cls')
         # Display the board
         print("  ", "  ".join(map(str, self.board[1:7])))
         print(self.board[0], " " * 17, self.board[7])
         print("  ", "  ".join(map(str, self.board[8:14])))
 
-    def end_game(self):
+    def end_game(self, board):
         # End the game and collect remaining seeds
-        self.board[0] += sum(self.board[1:7])
-        self.board[7] += sum(self.board[8:14])
+        board[0] += sum(board[1:7])
+        board[7] += sum(board[8:14])
         for i in range(1,14):
             if (i != 7):
-                self.board[i] = 0
+                board[i] = 0
 
     def is_game_over(self):
         # The game is over when one side of the board is empty
@@ -33,9 +33,9 @@ class KalahGame:
 
     def get_winner(self):
         # Determine the winner based on the number of seeds in Kalahs
-        if self.player_khala == self.computer_khala:
+        if self.board[self.player_khala] == self.board[self.computer_khala]:
             return "Egalité !"
-        if self.player_khala > self.computer_khala:
+        if self.board[self.player_khala] > self.board[self.computer_khala]:
             return "Vous avez gagné !"
         else:
             return "L'ordinateur a gagné !"
@@ -50,9 +50,12 @@ class KalahGame:
             else: 
                 return caseNumber
             
-    def move(self, kalah, enemy_kalah, selected_hole):
-        marbles = self.board[selected_hole]
-        self.board[selected_hole] = 0
+    def move(self, kalah, enemy_kalah, selected_hole, board, is_computer_call, depth):
+        #dans le cas ou on est à un niveau de profondeur inférieur au treshold
+        if(selected_hole == None):
+            return
+        marbles = board[selected_hole]
+        board[selected_hole] = 0
         #on ajoute 1 pour que i commence à 1
         deplacement = selected_hole
         while marbles > 0:
@@ -70,19 +73,25 @@ class KalahGame:
             #si la case est le kalah de l'adversaire ou la case initalement choisi on passe
             if(deplacement == enemy_kalah or deplacement == selected_hole):
                 continue
-            self.board[(deplacement)] += 1
+            board[(deplacement)] += 1
             marbles -= 1
             #si dernier tours
             if(marbles == 0):
                 #si la dernière bille est placée dans son kalah le joueur rejoue
                 if(deplacement == kalah):
-                    self.display_board()
-                    print("Vous rejouez")
-                    self.move(kalah, enemy_kalah, self.selectHole())
+                    #si la fonction à été appeler au tour du joueur
+                    if(not self.is_game_over()) : 
+                        if(not is_computer_call):
+                            self.display_board()
+                            print("Vous rejouez")
+                            self.move(kalah, enemy_kalah, self.selectHole(), board, False, depth -1)
+                        else: 
+                            print("computer simule un rejouer")
+                            self.move(kalah, enemy_kalah, self.computer_move(board, depth), board, True, depth -1)
                 #si la case avait une ou deux billes le joueur prend les billes dans sont kalah
-                elif(1 < self.board[deplacement] < 4):
-                    self.board[kalah] += self.board[deplacement]
-                    self.board[deplacement] = 0
+                elif(1 < board[deplacement] < 4):
+                    board[kalah] += board[deplacement]
+                    board[deplacement] = 0
                     j = 1
                     while True:
                         #recule d'une case à chaque fois que la case contient 1 ou 2 billes et les ajoute au kalah du joueur
@@ -96,25 +105,141 @@ class KalahGame:
                         if(deplacementJ == kalah or deplacementJ == enemy_kalah):
                             j += 1
                             continue
-                        if (1 < self.board[deplacementJ] < 4): 
-                            self.board[kalah] += self.board[deplacementJ]
-                            self.board[deplacementJ] = 0
+                        if (1 < board[deplacementJ] < 4): 
+                            board[kalah] += board[deplacementJ]
+                            board[deplacementJ] = 0
                             j += 1
                         else : 
                             break
     
-    def computer_move(self):
-        return 1 if self.computer_side == 0 else 8
+    def computer_move(self, computer_board, max_depth):
+        bestMove = None
+        bestValue = -float('inf')
+        print(max_depth)
+        if (max_depth <= 0):
+            return None
+        for hole in range(self.computer_khala +1, self.computer_khala + 7):
+            print("branche", hole)
+            #coupe la branche si la case est vide
+            if(computer_board[hole] == 0):
+                continue
+            #fait une copy du plateau pour ne pas modifier le plateau original
+            boardCopy = computer_board[:]
+            #simule un tour de jeu
+            self.move(self.computer_khala, self.player_khala, hole, boardCopy, True, max_depth -1)       
+            branche_value = self.alphaBeta(max_depth, boardCopy, False, -float('inf'), float('inf'))
+            bestValue = max(bestValue, branche_value)
+            if bestValue == branche_value:
+                bestMove = hole
+            return bestMove
+
+    def minMax(self, depth, board, ourTurn):
+        if(depth <= 0 or self.is_game_over()):
+            return self.evaluate_board(board)
+        #on prend le max pour soi meme
+        if(ourTurn):
+            bestValue = -float('inf')
+            for hole in range(self.computer_khala +1, self.computer_khala + 7):
+                if(board[hole] == 0):
+                    continue
+                boardCopy = board[:]
+                self.move(self.computer_khala, self.player_khala, hole, boardCopy, True, depth -1)
+                bestValue = max(bestValue, self.minMax(depth - 1, boardCopy, False))
+            return bestValue
+        #et le min pour l'adverssaire
+        else:
+            bestValue = float('inf')
+            for hole in range(self.player_khala +1, self.player_khala + 7):
+                if(board[hole] == 0):
+                    continue
+                boardCopy = board[:]
+                self.move(self.player_khala, self.computer_khala, hole, boardCopy, True, depth -1)
+                bestValue = min(bestValue, self.minMax(depth - 1, boardCopy, True))
+            return bestValue
+        
+    def alphaBeta(self, depth, board, ourTurn, alpha, beta):
+        if depth <= 0 or self.is_game_over():
+            return self.evaluate_board(board)
+        if ourTurn:  # Maximiseur
+            bestValue = -float('inf')
+            for hole in range(self.computer_khala + 1, self.computer_khala + 7):
+                if board[hole] == 0:
+                    continue
+                boardCopy = board[:]
+                self.move(self.computer_khala, self.player_khala, hole, boardCopy, True, depth - 1)
+                value = self.alphaBeta(depth - 1, boardCopy, False, alpha, beta)
+                bestValue = max(bestValue, value)
+                alpha = max(alpha, value)
+                if beta <= alpha:
+                    break  # Élagage Beta
+            return bestValue
+        else:  # Minimiseur
+            bestValue = float('inf')
+            for hole in range(self.player_khala + 1, self.player_khala + 7):
+                if board[hole] == 0:
+                    continue
+                boardCopy = board[:]
+                self.move(self.player_khala, self.computer_khala, hole, boardCopy, True, depth - 1)
+                value = self.alphaBeta(depth - 1, boardCopy, True, alpha, beta)
+                bestValue = min(bestValue, value)
+                beta = min(beta, value)
+                if beta <= alpha:
+                    break  # Élagage Alpha
+        return bestValue
+        
+    def evaluate_board(self, board):
+        if self.is_game_over():
+            self.end_game(board)
+            difference = board[self.computer_khala] - board[self.player_khala]
+            if difference > 0:
+                # Préférer gagner avec plus de graines
+                return float('inf') + difference 
+            elif difference < 0:
+                # Minimiser la perte
+                return float('-inf') + difference
+            else:
+                return 0  # Égalité
+        else:
+            kalah_difference = board[self.computer_khala] - board[self.player_khala]
+            position_value = 0
+
+            #on gagne des points à chaque fois qu'une case offre un avantage positionnel
+            for hole in range(self.computer_khala + 1, self.computer_khala + 7):
+                #si la case permet de rejouer
+                if (board[hole] == (7 - hole)):
+                    position_value += 5
+                #si la case permet de prendre les graines est d'entrainer une reaction en chaine :
+                #si la case à deux graines à offrir
+                elif (board[hole] == 2):
+                    position_value += 2
+                #si elle en à trois
+                elif (board[hole] == 3):
+                    position_value += 2.5
+
+            #on perd des points à chaque fois qu'une case offre un avantage stratégique à l'adversaire
+            for enemy_hole in range(self.player_khala + 1, self.player_khala + 7):
+                if (board[enemy_hole] == (7 - enemy_hole)):
+                    position_value -= 5
+                elif (board[enemy_hole] == 2):
+                    position_value -= 2
+                elif (board[enemy_hole] == 3):
+                    position_value -= 2.5
+
+            #plus on à de graines de notre coté par rapport à camp adversaire plus on gagne de points
+            position_value += (sum(board[self.computer_khala + 1: self.computer_khala +7]) - sum(board[self.player_khala + 1: self.player_khala +7]) *1.5)
+            return kalah_difference  * 2 + position_value
+            
 
     def start(self):
         self.display_board()
+        max_depth = 3 if self.level == 1 else 5 if self.level == 2 else 9 if self.level == 3 else None
         while True:
             
-            self.move(self.player_khala, self.computer_khala, self.selectHole())
+            self.move(self.player_khala, self.computer_khala, self.selectHole(), self.board, False, max_depth)
             if self.is_game_over():
                 break  
 
-            self.move(self.computer_khala, self.player_khala, self.computer_move())
+            self.move(self.computer_khala, self.player_khala, self.computer_move(self.board, max_depth), self.board, True, max_depth)
             if self.is_game_over():
                 break
 
@@ -123,7 +248,7 @@ class KalahGame:
             if(input("souhaiter vous sauvegarder la partie ? (O/N)") == "O"):
                 self.save_game()
                 return
-        self.end_game()
+        self.end_game(self.board)
         print("plateau final :")
         self.display_board()
         print(self.get_winner())
@@ -182,9 +307,6 @@ def load_JSON(filename="kalah_save.json"):
         game_state = json.load(file)
     return game_state["board"], game_state["level"], game_state["side"], game_state["player_khala"], game_state["computer_khala"]
 
-def minMax():
-    #à faire
-    return
     
 initalize_game()
 
